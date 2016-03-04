99d0353032865845989ea9cf6da57fc500b69934
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-03-04 16:04:39 -0300

    last change in contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/alissonp.medeiros@gmail.com",
+                url: "//formspree.io/empresaknex@gmail",
 
                 type: "POST",
                 data: {
